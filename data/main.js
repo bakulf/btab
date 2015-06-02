42fc34c577ea6efa9c29c648a66245ee9837e5db
baku <baku@mozilla.com>
2015-06-02 16:50:56 +0100

    Migration to foundation

--- data/main.js
+++ data/main.js
@@ -10,9 +10,6 @@ let Controller = {
 
   init: function() {
     this._filter = document.getElementById('filter');
-    this._filter.onchange = function() {
-      this.filterChanged();
-    }.bind(this);
 
     this._search = document.getElementById('search');
     this._search.onkeypress = function() {
@@ -20,11 +17,17 @@ let Controller = {
     }.bind(this);
 
     document.getElementById('settingsForm').onsubmit = function() {
-      return this.filterChanged();
+      return this.settingsSave();
     }.bind(this);
 
     document.getElementById('settingsAnchor').onclick = function() {
       this.settings();
+      return false;
+    }.bind(this);
+
+    document.getElementById('titleAnchor').onclick = function() {
+      this._activeURL = null;
+      this.refreshDataNeeded();
       return false;
     }.bind(this);
 
@@ -35,7 +38,7 @@ let Controller = {
         this._activeURL = aData.activeURL;
       }
 
-      this.refreshDataReceived();
+      this.refreshDataNeeded();
     }.bind(this));
 
     document.addEventListener('visibilitychange', function() {
@@ -47,14 +50,13 @@ let Controller = {
     self.port.emit('dataNeeded');
   },
 
-  filterChanged: function() {
+  settingsValidation: function() {
     if (this._filter.value.indexOf('http://') != 0 &&
         this._filter.value.indexOf('https://') != 0) {
-      $('#filterAlert').removeClass('hide');
+      $('#filterAlert').show();
       return false;
     }
-    
-    self.port.emit('filterChanged', this._filter.value);
+
     return true;
   },
 
@@ -64,37 +66,62 @@ let Controller = {
     }
 
     this._searchTimeout = setTimeout(function() {
-      this.refreshDataReceived();
+      this.refreshDataNeeded();
     }.bind(this), 300);
   },
 
   settings: function() {
-    $('#filterAlert').addClass('hide');
-    $('#settingsModal').modal({
-      keyboard: false,
-      backdrop: 'static'
-    });
+    $('#filterAlert').hide();
+    $('#settingsModal').foundation('reveal', 'open');
+
+    $(document).on('closed.fndtn.reveal', '[data-reveal]', function() {
+      if (!this.settingsSave()) {
+        this.settings();
+        $('#filterAlert').show();
+      }
+    }.bind(this));
   },
 
-  refreshDataReceived: function() {
+  settingsSave: function() {
+    if (!this.settingsValidation()) {
+      return false;
+    }
+
+    self.port.emit('filterChanged', this._filter.value);
+    return true;
+  },
+
+  refreshDataNeeded: function() {
     if (this._data.match != '') {
       this._filter.value = this._data.match
     } else {
       this.settings();
+      return;
     }
-  
+
     let container = document.getElementById('container');
     while(container.firstChild) {
       container.removeChild(container.firstChild);
     }
-  
+
     if (this._data.pages.length == 0) {
       let h3 = document.createElement('h3');
       h3.appendChild(document.createTextNode('Open some patch matching the filter URL'));
       container.appendChild(h3);
       return;
     }
-  
+
+    let pages = this.filterPages();
+
+    if (this._activeURL) {
+      this.showListAndPage(pages, container);
+      return;
+    }
+
+    this.showGrid(pages, container);
+  },
+
+  filterPages: function() {
     let pages = this._data.pages;
     let search = this._search.value;
     if (search != '') {
@@ -118,82 +145,109 @@ let Controller = {
       }
     }
 
-    if (pages.length == 0) {
+    return pages;
+  },
+
+  showGrid: function(aPages, aContainer) {
+    if (aPages.length == 0) {
       let h3 = document.createElement('h3');
       h3.appendChild(document.createTextNode('No results found'));
-      container.appendChild(h3);
-    } else {
-      let row;
-      for (let i = 0; i < pages.length; ++i) {
-        if (!(i % 4)) {
-          row = null;
-        }
-        if (!row) {
-          row = document.createElement('div');
-          row.setAttribute('class', 'row');
-          container.appendChild(row);
-        }
-    
-        let page = this.createPage(pages[i]);
-        row.appendChild(page);
-      }
+      aContainer.appendChild(h3);
+      return;
     }
-  
-    if (this._activeURL) {
-      for (let i = 0; i < this._data.pages.length; ++i) {
-        if (this._data.pages[i].url == this._activeURL) {
-          this.showPage(this._data.pages[i]);
-          break;
-        }
+
+    let row;
+    for (let i = 0; i < aPages.length; ++i) {
+      if (!(i % 4)) {
+        row = null;
       }
+
+      if (!row) {
+        row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        aContainer.appendChild(row);
+      }
+
+      let page = this.createPage(aPages[i]);
+      row.appendChild(page);
+    }
+  },
+
+  showListAndPage: function(aPages, aContainer) {
+    let mainRow = document.createElement('div');
+    mainRow.setAttribute('class', 'row');
+    aContainer.appendChild(mainRow);
+
+    let list = document.createElement('div');
+    list.setAttribute('class', 'row span3');
+    mainRow.appendChild(list);
+
+    let page = document.createElement('div');
+    page.setAttribute('class', 'row span9');
+    mainRow.appendChild(page);
+
+    let iframe = document.createElement('iframe');
+    iframe.setAttribute('src', this._activeURL);
+    page.appendChild(iframe);
+
+    if (aPages.length == 0) {
+      let h3 = document.createElement('h3');
+      h3.appendChild(document.createTextNode('No results found'));
+      list.appendChild(h3);
+    }
+
+    // TODO: scroll to the right position!
+    // TODO: activate the correct one
+    for (let i = 0; i < aPages.length; ++i) {
+      let row = document.createElement('div');
+      row.setAttribute('class', 'row');
+      list.appendChild(row);
+
+      let page = this.createPage(aPages[i]);
+      row.appendChild(page);
     }
   },
 
   createPage: function(aPage) {
     let div = document.createElement('div');
-    div.setAttribute('class', 'span3 well well-small');
-  
+    div.setAttribute('class', 'large-3');
+
     let anchor = document.createElement('a');
     anchor.setAttribute('href', '');
     anchor.setAttribute('title', aPage.title);
     div.appendChild(anchor);
-  
+
     let title = document.createElement('h4');
     title.appendChild(document.createTextNode(aPage.title));
     anchor.appendChild(title);
-  
+
     let img = document.createElement('img');
     img.setAttribute('src', aPage.thumbnail);
     anchor.appendChild(img);
-  
+
     let closeAnchor = document.createElement('a');
     closeAnchor.appendChild(document.createTextNode('close'));
     closeAnchor.setAttribute('href', '');
     closeAnchor.setAttribute('title', 'Close tab');
     div.appendChild(closeAnchor);
-  
+
     anchor.onclick = function() {
       this.showPage(aPage);
       return false;
     }.bind(this);
-  
+
     closeAnchor.onclick = function() {
       self.port.emit('closeURL', aPage.url);
       return false;
     }
-  
+
     return div;
   },
 
   showPage: function(aPage) {
-    // TODO
-  },
-
-  alert: function(aMsg) {
-    $('#alertMessage').text(aMsg);
-    $('#alertModal').modal('show');
+    this._activeURL = aPage.url;
+    this.refreshDataNeeded();
   }
 };
 
 Controller.init();
-
