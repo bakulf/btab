055100422890c9d89261b008670c667ffbbfba22
baku <baku@ippolita.net>
2015-06-04 07:01:42 +0100

    Browser API enabled

--- data/main.js
+++ data/main.js
@@ -11,6 +11,19 @@ let Controller = {
   _data: null,
   _activeURL: null,
 
+  _pages: [
+    { name: 'settingsPage',
+      init: function(aObj) {} },
+    { name: 'gridPageNoResult',
+      init: function(aObj) { $("#searchGridResult").text(aObj._search.value); } },
+    { name: 'gridPageNoTab',
+      init: function(aObj) {} },
+    { name: 'gridPageRest',
+      init: function(aObj) { aObj.gridPageRestInit(); } },
+    { name: 'listPage',
+      init: function(aObj) { aObj.listPageInit(); } } ],
+  _currentPage: null,
+
   init: function() {
     this._filterURL = document.getElementById('filterURL');
     this._filterName = document.getElementById('filterName');
@@ -37,7 +50,7 @@ let Controller = {
           required: true,
           message: 'It has to be a valid filter URL',
           test: function() {
-            var filter = document.getElementById("filterURL");
+            let filter = document.getElementById("filterURL");
             return filter.value.indexOf('http://') == 0 ||
                    filter.value.indexOf('https://') == 0;
           }
@@ -46,7 +59,7 @@ let Controller = {
           required: true,
           message: 'It has to be higher then 0',
           test: function() {
-            var filter = document.getElementById("filterMaxPages");
+            let filter = document.getElementById("filterMaxPages");
             return parseInt(filter.value) > 0;
           }
         }
@@ -105,24 +118,26 @@ let Controller = {
     self.port.emit('dataNeeded');
   },
 
-  searchChanged: function() {
-    if (this._searchTimeout) {
-      clearTimeout(this._searchTimeout);
+  openURL: function(aURL) {
+    this._activeURL = aURL;
+    if (this._currentPage != 'listPage') {
+      this.listPage();
+      return;
     }
 
-    this._searchTimeout = setTimeout(function() {
-      $('.button-search').toggleClass('icon-search').toggleClass('icon-repeat');
-      this.refreshDataNeeded();
-    }.bind(this), 300);
+    this.updateBrowserController();
   },
 
+  // Pages --------------------------------------------------------------------
+
   showPage: function(aPage) {
-    let pages = [ 'settingsPage', 'gridPageNoResult', 'gridPageNoTab', 'gridPageRest', 'listPage' ];
-    for (let i = 0; i < pages.length; ++i) {
-      if (aPage == pages[i]) {
-        $("#" + pages[i]).show();
+    for (let i = 0; i < this._pages.length; ++i) {
+      if (aPage == this._pages[i].name) {
+        this._currentPage = this._pages[i].name;
+        $("#" + this._pages[i].name).show();
+        this._pages[i].init(this);
       } else {
-        $("#" + pages[i]).hide();
+        $("#" + this._pages[i].name).hide();
       }
     }
   },
@@ -136,77 +151,21 @@ let Controller = {
     this.refreshDataNeeded();
   },
 
-  updateTitle: function() {
-    $("#filterTitleName").text(this._filterName.value);
-    $("title").text("B:Tab - " + this._filterName.value);
-  },
-
-  settingsSave: function() {
-    this.updateTitle();
-    self.port.emit('filterChanged', { url: this._filterURL.value,
-                                      name: this._filterName.value,
-                                      maxPages: parseInt(this._filterMaxPages.value) });
-    this.mainPage();
-  },
-
-  refreshDataNeeded: function() {
-    if (this._data.match != '' || this._data.name != '') {
-      this._filterURL.value = this._data.match;
-      this._filterName.value = this._data.name;
-      this._filterMaxPages.value = this._data.maxPages;
-      this.updateTitle();
-    } else {
-      this.settings();
-      return;
-    }
-
-    if (this._activeURL) {
-      this.listPage(false);
-    } else {
-      this.gridPage();
-    }
-  },
-
-  filterPages: function() {
-    let pages = this._data.pages;
-    let search = this._search.value;
-    if (search != '') {
-      search = search.toLocaleLowerCase();
-
-      let parts = search.split(' ');
-      for (let p = 0; p < parts.length && pages.length; ++p) {
-        if (!parts[p].length) {
-          continue;
-        }
-
-        let tmp = [];
-        for (let i = 0; i < pages.length; ++i) {
-          if (pages[i].title.toLocaleLowerCase().indexOf(parts[p]) != -1 ||
-              pages[i].url.indexOf(parts[i]) != -1) {
-            tmp.push(pages[i]);
-          }
-        }
-
-        pages = tmp;
-      }
-    }
-
-    return pages;
-  },
-
   gridPage: function() {
     if (this._data.pages.length == 0) {
       this.showPage('gridPageNoTab');
       return;
     }
 
-    let pages = this.filterPages();
-    if (pages.length == 0) {
-      $("#searchGridResult").text(this._search.value);
+    if (this._filteredPages.length == 0) {
       this.showPage('gridPageNoResult');
       return;
     }
 
+    this.showPage('gridPageRest');
+  },
+
+  gridPageRestInit: function() {
     let container = document.getElementById('gridPageRest');
     while(container.firstChild) {
       container.removeChild(container.firstChild);
@@ -228,8 +187,8 @@ let Controller = {
       container.appendChild(h);
     }
 
-    for (let i = 0; i < pages.length; ++i) {
-      let page = this.createPage(pages[i]);
+    for (let i = 0; i < this._filteredPages.length; ++i) {
+      let page = this.createPage(this._filteredPages[i]);
 
       let div = document.createElement('div');
       div.setAttribute('class', 'large-3 columns end');
@@ -237,8 +196,164 @@ let Controller = {
 
       container.appendChild(div);
     }
+  },
 
-    this.showPage('gridPageRest');
+  listPageInit: function() {
+    let container = document.getElementById('listPageList');
+    while(container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+
+    if (this._search.value != "") {
+      let h = document.createElement('h3');
+      h.setAttribute('class', 'search-query');
+      h.appendChild(document.createTextNode('your searched for: '));
+      container.appendChild(h);
+
+      let em = document.createElement('em');
+      em.appendChild(document.createTextNode(this._search.value));
+      h.appendChild(em);
+    } else {
+      let h = document.createElement('h3');
+      h.setAttribute('class', 'search-query');
+      h.appendChild(document.createTextNode('Your tabs'));
+      container.appendChild(h);
+    }
+
+    for (let i = 0; i < this._filteredPages.length; ++i) {
+      let page = this.createPage(this._filteredPages[i]);
+      container.appendChild(page);
+    }
+
+    this.updateBrowserController();
+  },
+
+  listPage: function(aPartial) {
+    if (this._data.pages.length == 0) {
+      dump("BTAB listPage: This seems a bug!\n");
+      this.mainPage();
+      return;
+    }
+
+    this.showPage('listPage');
+  },
+
+  // UI callbacks -------------------------------------------------------------
+
+  searchChanged: function() {
+    if (this._searchTimeout) {
+      clearTimeout(this._searchTimeout);
+    }
+
+    this._searchTimeout = setTimeout(function() {
+      $('.button-search').toggleClass('icon-search').toggleClass('icon-repeat');
+      this.refreshDataNeeded();
+    }.bind(this), 300);
+  },
+
+  settingsSave: function() {
+    this.updateTitle();
+    self.port.emit('filterChanged', { url: this._filterURL.value,
+                                      name: this._filterName.value,
+                                      maxPages: parseInt(this._filterMaxPages.value) });
+    this.mainPage();
+  },
+
+  urlChanged: function(aOld, aNew) {
+    self.port.emit('urlChanged', { oldURL: aOld, newURL: aNew });
+
+    for (let i = 0; i < this._data.pages.length; ++i) {
+      if (this._data.pages[i].url == aOld) {
+        this._data.pages[i].url = aNew;
+        break;
+      }
+    }
+
+    $(".single-item").each(function() {
+      if ($(this).attr("data-url") == aOld) {
+        $(this).attr("data-url", aNew);
+        // What about duplicates?!?
+      }
+    });
+  },
+
+  titleChanged: function(aUrl, aTitle) {
+    self.port.emit('titleChanged', { url: aUrl, title: aTitle });
+
+    for (let i = 0; i < this._data.pages.length; ++i) {
+      if (this._data.pages[i].url == aUrl) {
+        this._data.pages[i].title = aTitle;
+        break;
+      }
+    }
+
+    $(".single-item").each(function() {
+      if ($(this).attr("data-url") == aUrl) {
+        $(this).find('.single-item-title').text(aTitle);
+      }
+    });
+  },
+
+  closeURL: function(aUrl) {
+    self.port.emit('closeURL', aUrl);
+    for (let i = 1; i < this._data.pages.length; ++i) {
+      if (this._data.pages[i].url == aUrl) {
+        this._data.pages.splice(i, 1);
+        break;
+      }
+    }
+
+    $(".single-item").each(function() {
+      if ($(this).attr("data-url") == aUrl) {
+        $(this).remove();
+      }
+    });
+  },
+
+  loadingStart: function(aIframe) {
+    $("#pageReload").addClass('icon-repeat');
+  },
+
+  loadingEnd: function(aIframe) {
+    $("#pageReload").removeClass('icon-repeat');
+  },
+
+  // UI methods ---------------------------------------------------------------
+
+  updateTitle: function() {
+    $("#filterTitleName").text(this._filterName.value);
+    $("title").text("B:Tab - " + this._filterName.value);
+  },
+
+  createIframe: function() {
+    let url = this._activeURL;
+    let iframe = document.createElement('iframe');
+    iframe.setAttribute('mozbrowser', true);
+
+    iframe.addEventListener('mozbrowserlocationchange', function(a) {
+      if (a.detail != url) {
+        this.urlChanged(url, a.detail);
+        url = a.detail;
+        if (this._activeURL == url) {
+          this._activeURL = a.detail;
+          this.updateBrowserControler();
+        }
+      }
+    }.bind(this));
+
+    iframe.addEventListener('mozbrowsertitlechange', function(a) {
+      this.titleChanged(url, a.detail);
+    }.bind(this));
+
+    iframe.addEventListener('mozbrowserloadstart', function(a) {
+      this.loadingStart(iframe);
+    }.bind(this));
+
+    iframe.addEventListener('mozbrowserloadend', function(a) {
+      this.loadingEnd(iframe);
+    }.bind(this));
+
+    return iframe;
   },
 
   showIframe: function() {
@@ -255,7 +370,7 @@ let Controller = {
     }
 
     if (!iframe) {
-      iframe = document.createElement('iframe');
+      iframe = this.createIframe();
       container.appendChild(iframe);
     }
 
@@ -268,120 +383,8 @@ let Controller = {
     while (container.children.length > parseInt(this._filterMaxPages.value)) {
       container.removeChild(container.firstChild);
     }
-  },
 
-  listPage: function(aPartial) {
-    if (this._data.pages.length == 0) {
-      dump("BTAB: This seems a bug!\n");
-      this.mainPage();
-      return;
-    }
-
-    let visible = $("#listPage").is(":visible");
-    this.showPage('listPage');
-    this.updateListPage();
-
-    if (!aPartial || !visible) {
-      let container = document.getElementById('listPageList');
-      while(container.firstChild) {
-        container.removeChild(container.firstChild);
-      }
-
-      let pages = this.filterPages();
-
-      if (this._search.value != "") {
-        let h = document.createElement('h3');
-        h.setAttribute('class', 'search-query');
-        h.appendChild(document.createTextNode('your searched for: '));
-        container.appendChild(h);
-
-        let em = document.createElement('em');
-        em.appendChild(document.createTextNode(this._search.value));
-        h.appendChild(em);
-      } else {
-        let h = document.createElement('h3');
-        h.setAttribute('class', 'search-query');
-        h.appendChild(document.createTextNode('Your tabs'));
-        container.appendChild(h);
-      }
-
-      for (let i = 0; i < pages.length; ++i) {
-        let page = this.createPage(pages[i]);
-        container.appendChild(page);
-      }
-    }
-  },
-
-  updateListPage: function() {
-    this.showIframe();
-    $("#pageURL").attr('value', this._activeURL);
-
-    if (this.canGoBack()) {
-      $("#pageBack").removeClass('disabled');
-    } else {
-      $("#pageBack").addClass('disabled');
-    }
-
-    if (this.canGoForward()) {
-      $("#pageForward").removeClass('disabled');
-    } else {
-      $("#pageForward").addClass('disabled');
-    }
-  },
-
-  resize: function() {
-    // Just for the list page
-    if (!$("#listPage").is(":visible")) {
-      return;
-    }
-
-    var height_screen = $(window).height();
-    var width_screen = $(window).width();
-    $('.content-wrapper').css('height', height_screen - $('.content-wrapper').offset().top);
-    $('#tabs').css('height', height_screen - $('#tabs').offset().top);
-    $('iframe').css('width', width_screen - $('iframe').offset().left);
-    $('iframe').css('height', height_screen - $('iframe').offset().top);
-  },
-
-  createPage: function(aPage) {
-    let item = document.createElement('div');
-    item.setAttribute('class', 'single-item');
-
-    let title = document.createElement('h2');
-    title.appendChild(document.createTextNode(aPage.title));
-    item.appendChild(title);
-
-    let ul = document.createElement('ul');
-    ul.setAttribute('class', 'toolbar'); // TODO active
-    item.appendChild(ul);
-
-    let li = document.createElement('li');
-    li.setAttribute('class', 'item icon-star');
-    ul.appendChild(li);
-
-    li.onclick = function() {
-      // TODO
-      return false;
-    }.bind(this);
-
-    li = document.createElement('li');
-    li.setAttribute('class', 'item icon-trash');
-    ul.appendChild(li);
-
-    li.onclick = function(e) {
-      self.port.emit('closeURL', aPage.url);
-      e.stopPropagation();
-      return false;
-    }.bind(this);
-
-    item.onclick = function(e) {
-      this._activeURL = aPage.url;
-      this.listPage(true);
-      e.stopPropagation();
-      return false;
-    }.bind(this);
-
-    return item;
+    this._iframe = iframe;
   },
 
   canGoBack: function() {
@@ -409,7 +412,7 @@ let Controller = {
     }
 
     this._activeURL = url;
-    this.updateListPage();
+    this.updateBrowserController();
   },
 
   goForward: function() {
@@ -419,11 +422,138 @@ let Controller = {
     }
 
     this._activeURL = url;
-    this.updateListPage();
+    this.updateBrowserController();
   },
 
   goReload: function() {
-    $("iframe").attr('src', this._activeURL);
+    if (!this._iframe) {
+      dump("BTAB goReaload: This seems a bug!\n");
+      return;
+    }
+
+    this._iframe.reload();
+  },
+
+  createPage: function(aPage) {
+    let item = document.createElement('div');
+    item.setAttribute('class', 'single-item');
+    item.setAttribute('data-url', aPage.url);
+
+    let title = document.createElement('h2');
+    title.setAttribute('class', 'single-item-title');
+    title.appendChild(document.createTextNode(aPage.title != "" ? aPage.title : aPage.url));
+    item.appendChild(title);
+
+    let ul = document.createElement('ul');
+    ul.setAttribute('class', 'toolbar'); // TODO active
+    item.appendChild(ul);
+
+    let li = document.createElement('li');
+    li.setAttribute('class', 'item icon-star');
+    ul.appendChild(li);
+
+    li.onclick = function() {
+      // TODO
+      return false;
+    }.bind(this);
+
+    li = document.createElement('li');
+    li.setAttribute('class', 'item icon-trash');
+    ul.appendChild(li);
+
+    li.onclick = function(e) {
+      this.closeURL(aPage.url);
+      e.stopPropagation();
+      return false;
+    }.bind(this);
+
+    item.onclick = function(e) {
+      this.openURL(aPage.url);
+      e.stopPropagation();
+      return false;
+    }.bind(this);
+
+    return item;
+  },
+
+  resize: function() {
+    // Just for the list page
+    if (this._currentPage != 'listPage') {
+      return;
+    }
+
+    let height_screen = $(window).height();
+    let width_screen = $(window).width();
+    $('.content-wrapper').css('height', height_screen - $('.content-wrapper').offset().top);
+    $('#tabs').css('height', height_screen - $('#tabs').offset().top);
+    $('iframe').css('width', width_screen - $('#iframe-content').offset().left);
+    $('iframe').css('height', height_screen - $('#iframe-content').offset().top);
+  },
+
+  updateBrowserController: function() {
+    this.showIframe();
+    $("#pageURL").attr('value', this._activeURL);
+
+    if (this.canGoBack()) {
+      $("#pageBack").removeClass('disabled');
+    } else {
+      $("#pageBack").addClass('disabled');
+    }
+
+    if (this.canGoForward()) {
+      $("#pageForward").removeClass('disabled');
+    } else {
+      $("#pageForward").addClass('disabled');
+    }
+  },
+
+  // Data received ------------------------------------------------------------
+
+  refreshDataNeeded: function() {
+    if (this._data.match == '' || this._data.name == '') {
+      this.settings();
+      return;
+    }
+
+    this._filterURL.value = this._data.match;
+    this._filterName.value = this._data.name;
+    this._filterMaxPages.value = this._data.maxPages;
+    this.updateTitle();
+
+    this.filterPages();
+
+    if (this._activeURL) {
+      this.listPage();
+    } else {
+      this.gridPage();
+    }
+  },
+
+  filterPages: function() {
+    let pages = this._data.pages;
+    let search = this._search.value;
+    if (search != '') {
+      search = search.toLocaleLowerCase();
+
+      let parts = search.split(' ');
+      for (let p = 0; p < parts.length && pages.length; ++p) {
+        if (!parts[p].length) {
+          continue;
+        }
+
+        let tmp = [];
+        for (let i = 0; i < pages.length; ++i) {
+          if (pages[i].title.toLocaleLowerCase().indexOf(parts[p]) != -1 ||
+              pages[i].url.indexOf(parts[p]) != -1) {
+            tmp.push(pages[i]);
+          }
+        }
+
+        pages = tmp;
+      }
+    }
+
+    this._filteredPages = pages;
   },
 };
 
